a223d7055ab18660ab66eb6144c7b7b217bd531b
Andreas Maser <andreasmaser@me.com>
2023-04-21 09:21:25 +0200

    Initial Commit

---
+++ ToDo_AMA/ToDo_AMAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_AMAApp.swift
+//  ToDo_AMA
+//
+//  Created by Andreas Maser on 21.04.23.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_AMAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
